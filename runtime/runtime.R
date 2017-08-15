6c4abc3481f561c832d8ed47c5843a1f96846f4a
Ben Zhang <benzh@cs.berkeley.edu>
2017-08-14 17:05:12 -0700

    add jetstream darknet data

--- runtime/runtime.R
+++ runtime/runtime.R
@@ -1,19 +1,25 @@
 #!/usr/bin/env Rscript
 
-library(reshape2)
+library(cowplot)
 library(ggsci)
+library(reshape2)
 
-f <- "mot.runtime.csv"  ## darknet.runtime.csv
+f <- "darknet.runtime.csv"  ## darknet.runtime.csv
 runtime.data <- read.csv(f)
-legends <- c("time", "AWStream", "Streaming over TCP", "Streaming over UDP")
+legends <- c("time", "AWStream", "JetStream",
+             "Streaming over TCP",
+             "Streaming over UDP")
 
 ## Create a test plot with proper size
-dev.new(width=3.2, height=2.4)
+dev.new(width=6, height=2.4)
 
 ##
 ## Bandwidth (throughput)
 ##
-bw <- runtime.data[,c("time", "ads.throughput", "tcp.throughput", "udp.throughput")]
+bw <- runtime.data[,c("time", "ads.throughput",
+                      "jet.throughput",
+                      "tcp.throughput",
+                      "udp.throughput")]
 names(bw) <- legends
 bw <- melt(bw, id.vars="time")
 bw$value <- bw$value / 1000
@@ -33,19 +39,21 @@ bw.plot
 ##
 ## latency plot
 ##
-latency <- runtime.data[,c("time", "ads.latency", "tcp.latency", "udp.latency")]
+latency <- runtime.data[,c("time", "ads.latency", "jet.latency",
+                           "tcp.latency",
+                           "udp.latency")]
 names(latency) <- legends
 latency <- melt(latency, id.vars="time")
 latency$value <- latency$value / 1000
 
 ## Now we break it into two parts
 l <- latency
-split <- 1
+split <- 2.0
 
-l$value[which(l$value > split)[1]] <- 0.99
-l$value[which(l$value > split)[1]] <- 1.01
-l$value[rev(which(l$value > split))[1]] <- 0.99
-l$value[rev(which(l$value > split))[1]] <- 1.01
+l$value[which(l$value > split)[1]] <- 1.99
+l$value[which(l$value > split)[1]] <- 2.01
+l$value[rev(which(l$value > split))[1]] <- 1.99
+l$value[rev(which(l$value > split))[1]] <- 2.01
 l$value[l$time == 400 & l$variable == "Streaming over TCP"] <- NA
 
 l$mask <- 1
@@ -55,7 +63,7 @@ transform <- function(i) log10(i) + split + 1
 inverse <- function(i) 10 ^ (i - split - 1)
 l$value[l$mask == 0] <- transform(l$value[l$mask == 0])
 
-breaks <- c(0, 0.5, 1.0, transform(1.0), transform(10.0), transform(100))
+breaks <- c(0, 1.0, 2.0, transform(2.0), transform(10.0), transform(100))
 labels <- breaks
 labels[labels > split] <- inverse(labels[labels > split])
 labels <- sapply(labels, function(l) if (l == 100) {
@@ -82,7 +90,10 @@ latency.plot
 ##
 ## accuracy plot
 ##
-accuracy <- runtime.data[,c("time", "ads.accuracy", "tcp.accuracy", "udp.accuracy")]
+accuracy <- runtime.data[,c("time", "ads.accuracy",
+                            "jet.accuracy",
+                            "tcp.accuracy",
+                            "udp.accuracy" )]
 names(accuracy) <- legends
 accuracy <- melt(accuracy, id.vars="time")
 accuracy.plot <- ggplot(accuracy, aes(x=time, y=value, colour=variable, shape=variable)) +
@@ -98,18 +109,18 @@ accuracy.plot <- ggplot(accuracy, aes(x=time, y=value, colour=variable, shape=va
 accuracy.plot
 
 ##
-## Final plot
+## Final plot uses cowplot to arrange everything
 ##
-pdf("runtime-mot-verticle.pdf", width=4, height=6.9)
-multiplot(bw.plot + theme(legend.position="none"),
-          latency.plot + theme(legend.position="none"),
-          accuracy.plot + theme(legend.position="none"),
-          cols=1)
-dev.off()
+pcol <- plot_grid(bw.plot + theme(legend.position="none"),
+                  latency.plot + theme(legend.position="none"),
+                  accuracy.plot + theme(legend.position="none"),
+                  align = 'vh', ncol = 1)
 
-pdf("runtime-legend.pdf", width=10, height=2)
-bw.plot + ylab("") + theme(legend.position="top",
-                           legend.key = element_rect(size = unit(30, 'lines')),
-                           legend.key.size = unit(4, 'lines'),
-                           legend.title = element_blank())
+legend <- get_legend(bw.plot + theme(legend.position="top",
+                                     legend.title = element_blank()))
+p <- plot_grid(legend, pcol, ncol = 1, rel_heights = c(.1, 1))
+
+
+pdf("runtime-darknet-verticle.pdf", width=7, height=7)
+p
 dev.off()
